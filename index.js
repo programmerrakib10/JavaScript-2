1d1e015128245ead3e693aba3802672731badb11
programmerrakib10 <biplobjak7@gmail.com>
2023-11-18 11:23:05 +0300

    JavaScript Class2 added

---
+++ index.js
@@ -0,0 +1,45 @@
+// Modulus  2tar vitor vag kora gele 0 hobe  r tanahola vagses e ja thake tai hobe 
+
+let num1 = 60
+let num2 = 30
+console.log(num1 % num2);
+
+// Addition & Increment & Decrement
+// +(Additon) sudhu matro jog korbe & ++(Increment) akta kore valu barabe 
+console.log(num1 + num2);
+
+num1 = 74;
+num1++;
+num1++;
+num1++;
+num1--;
+console.log("Increment",num1)
+
+// Multiplication & Exponentiation
+num1 = 13
+num2 = 7
+console.log("Multiplication", num1 * num2)
+console.log("Exponentiation", num1 ** num2)
+
+
+
+let math = 80;
+let english = 80;
+let science = 80;
+
+let averageMarks = (math + english + science) / 3;
+
+console.log("Average Markes:", averageMarks)
+ if (averageMarks >= 90) {
+    console.log("A+", averageMarks)
+ } else if (averageMarks >= 80) {
+    console.log("A", averageMarks)
+ } else if (averageMarks >= 60) {
+    console.log("B+", averageMarks)}
+ else if (averageMarks >= 50) {
+        console.log("C", averageMarks)}
+ else if (averageMarks >= 40) {
+            console.log("D", averageMarks)}  
+ else {
+    console.log("failed", averageMarks)
+   }           
---
+++ operators.js
@@ -0,0 +1,44 @@
+let user_name = "javascript";
+let password = "operators";
+let age = 20;
+
+if (user_name === "javascript") {
+    console.log("user can enter website")
+}
+
+// logical Operators
+// OR -> ||
+// AND -> &&
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+console.log(true || false || false || false ||false)
+
+if (user_name == "javascrikpt" || password == "operatorss" || age == 20) {
+    console.log("user can entire dashboard")
+} else{
+    console.log("wrong details")
+}
+
+// And er kaj kortre hobe akon 
+
+// AND er jonno j kono akta false thaklei seita false hoia jabe
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+//example 
+if (user_name == "javascript" && password == "operators" && age == 20) {
+    console.log ("user can entire dashboard")
+} else {
+    console.log("Wrong Details")
+}
+//  Ternary Operator
+user_name == javascript ? console.log("dark mode is on") : console.log("dark mpode is disabled")
+
+// problem Solving
+
